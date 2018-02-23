83f0265a2f73a0384cca8310cfa08a4696323dec
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-23 15:59:07 -0500

    add search view model

---
+++ src/ViewModels/SearchViewModel.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pokedex.Models;
+
+namespace Pokdex.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string SearchString { get; set; }
+        public IEnumerable<Pokemon> Pokemons { get; set; }
+        public int AmountOfHits { get; set; }
+        public string SelectedWeakness { get; set; }
+        public IEnumerable<string> SelectedWeaknesses { get; set; }
+        public IEnumerable<SelectListItem> AvailableWeaknesses { get; set; }
+
+        public SearchViewModel()
+        {
+            Pokemons = new List<Pokemon>();
+            SelectedWeaknesses = new List<string>();
+            AvailableWeaknesses = new List<SelectListItem>();
+        }
+    }
+}
