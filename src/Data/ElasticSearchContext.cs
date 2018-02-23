5ce9b665b06ba8f191b88d06901cbe81ea729572
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-23 15:46:42 -0500

    Add ElasticSarch Data Context and initial Seed

---
+++ src/Data/ElasticSearchContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Nest;
+using Pokedex.Models;
+
+namespace Pokedex.Data
+{
+    public class ElasticSearchContext : IElasticSearchContext
+    {
+        private static Uri node;
+
+        public ElasticClient GetClient()
+        {
+            node = new Uri("http://172.17.0.7:9200");
+            settings.DefaultIndex("pokemon");
+            var client = new ElasticClient(settings);
+            return client;
+        }
+
+        private static readonly ConnectionSettings settings = new ConnectionSettings();
+
+        private ElasticSearchContext() { }
+
+        public static ConnectionSettings Settings
+        {
+            get
+            {
+                return new ConnectionSettings(node);
+            }
+        }
+    }
+}
---
+++ src/Data/JsonDataConvertor.cs
@@ -0,0 +1,22 @@
+﻿using ElasticSearchWebAppTest.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokedex.Data
+{
+    public class JsonDataConvertor
+    {
+        public List<Pokemon.RootObject> JsonToPokemonList()
+        {
+            string dataPath = @"Data/pokedex.json";
+            string json = File.ReadAllText(dataPath);
+            var listWithPokemonRoots = JsonConvert.DeserializeObject<List<Pokemon.RootObject>>(json);
+
+            return listWithPokemonRoots;
+        }
+    }
+}
