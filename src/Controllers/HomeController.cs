0ddf7b94e4efcd21cceeb1c1795332f12101d282
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-23 15:50:26 -0500

    add seed and home index

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -4,34 +4,69 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Nest;
 using Pokedex.Models;
+using Pokedex.Data;
+using Pokedex.ViewModels;
+
 
 namespace Pokedex.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Uri _node;
+        private static ConnectionSettings _settings;
+        private static ElasticClient _client;
+
+        public HomeController()
+        {
+            _node = new Uri("http://localhost:9200");
+            _settings = new ConnectionSettings(_node);
+            _settings.DefaultIndex("pokemon");
+            _client = new ElasticClient(_settings);
+        }
+
         public IActionResult Index()
         {
-            return View();
+            if (_client.IndexExists("pokemon").Exists == false)
+            {
+
+                var indexDescriptor = new CreateIndexDescriptor("pokemon")
+                        .Mappings(ms => ms
+                            .Map<Pokemon>(m => m.AutoMap()
+                            
+                            ));
+
+                _client.CreateIndex("pokemon", i => indexDescriptor);
+
+
+                var convertor = new JsonDataConvertor();
+                var listWithPokemonRoots = convertor.JsonToPokemonList();
+
+                foreach (var pokemonRoot in listWithPokemonRoots)
+                {
+                    var pokemon = new Pokemon()
+                    {
+                        Id = pokemonRoot.id,
+                        Name = pokemonRoot.name,
+                        Type = pokemonRoot.type,
+                        Weaknesses = pokemonRoot.weaknesses,
+                    };
+
+                    var result = _client.Bulk(b => b
+                        .Index<Pokemon>(i => i.Document(pokemon))
+                    );
+                }
+            }
+
+            var model = new SearchViewModel()
+            {
+            };
+
+            return View(model);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
